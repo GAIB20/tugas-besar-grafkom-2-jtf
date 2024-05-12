65df61150420b26e59916d0c0c5eafc303a7b722
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2024-05-13 00:36:54 +0700

    refactor: spacing

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,26 @@
-import './style.css'
-import {Tweakpane} from "./components/tweakpane.ts";
+import './style.css';
+import { Tweakpane } from './components/tweakpane.ts';
 import { WebGL } from './core/webgl.ts';
 import { Scene } from './core/scene.ts';
 
 new Tweakpane();
 
 document.addEventListener('DOMContentLoaded', function () {
-    loadContent();
+  loadContent();
 });
 
 const loadContent = () => {
-    const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-    if (!canvas) return;
+  const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+  if (!canvas) return;
 
-    const gl = canvas.getContext('webgl');
-    if (!gl) return;
+  const gl = canvas.getContext('webgl');
+  if (!gl) return;
 
-    // TODO: Initialize WebGL here
-    const webGL = new WebGL(gl, canvas);
-    const scene =  new Scene(webGL);
+  // TODO: Initialize WebGL here
+  const webGL = new WebGL(gl, canvas);
+  const scene = new Scene(webGL);
 
-    // scene.loadGLTF("../test-data/sample.gltf");
+  // scene.loadGLTF("../test-data/sample.gltf");
 
-    scene.startRender();
-}
+  scene.startRender();
+};
