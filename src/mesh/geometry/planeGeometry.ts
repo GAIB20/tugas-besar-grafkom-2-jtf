7ecef230d506c20e2cafbb11765fb9a937dae0a9
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2024-05-13 10:36:53 +0700

    chore: set indices on plane geometry

--- src/mesh/geometry/planeGeometry.ts
+++ src/mesh/geometry/planeGeometry.ts
@@ -2,13 +2,8 @@ import { BufferAttribute } from './bufferAttribute';
 import { BufferGeometry } from './bufferGeometry';
 
 export class PlaneGeometry extends BufferGeometry {
-  width: number;
-  height: number;
-
   constructor(width = 1, height = 1) {
     super();
-    this.width = width;
-    this.height = height;
     const hw = width / 2,
       hh = height / 2;
     const vertices = new Float32Array([
@@ -23,15 +18,11 @@ export class PlaneGeometry extends BufferGeometry {
       hh,
       -hw,
       0,
-      hh,
-      -hw,
-      0,
-      -hh,
-      hw,
-      0,
       hh
     ]);
+    const indices = new Uint16Array([0, 1, 2, 2, 3, 0]);
     this.setAttribute('position', new BufferAttribute(vertices, 3));
+    this.setIndices(new BufferAttribute(indices, 1));
     this.calculateNormals();
   }
 }
