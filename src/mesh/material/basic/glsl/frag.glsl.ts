cc9dd3a9db4d1513b2e164650beb999649b4ba5c
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2024-05-14 18:09:35 +0700

    chore: update basic glsl

--- src/mesh/material/basic/glsl/frag.glsl.ts
+++ src/mesh/material/basic/glsl/frag.glsl.ts
@@ -1,4 +1,5 @@
-export default `precision highp float;
+export default `
+precision highp float;
 
 uniform vec4 u_diffuseColor;
 uniform sampler2D u_diffuseTexture;
@@ -7,7 +8,11 @@ uniform bool u_useTexture;
 varying vec2 v_texcoord;
 
 void main() {
-    vec4 texColor = texture2D(u_diffuseTexture, v_texcoord);
-    vec4 color = mix(u_diffuseColor, texColor, float(u_useTexture));
+    vec4 color = u_diffuseColor;
+    if (u_useTexture) {
+        vec4 texColor = texture2D(u_diffuseTexture, v_texcoord);
+        color = mix(color, texColor, float(u_useTexture));
+    }
     gl_FragColor = color;
-}`;
+}
+`;
