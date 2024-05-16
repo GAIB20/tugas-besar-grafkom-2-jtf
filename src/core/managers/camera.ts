c54268af4a932f0a32d3c83367dfdd550efe8ba8
Fajar Herawan <112335075+fajarmhrwn@users.noreply.github.com>
2024-05-16 20:50:32 +0700

    Feat: Camera oblique and perspective

--- src/core/managers/camera.ts
+++ src/core/managers/camera.ts
@@ -1,10 +1,13 @@
 import { Camera } from '../../camera/Camera';
+import { ObliqueCamera } from '../../camera/ObliqueCamera';
 import { OrthographicCamera } from '../../camera/OrthographicCamera';
+import { PerspectiveCamera } from '../../camera/PerspectiveCamera';
 
 export class CameraManager {
   camera: Camera;
   private orthographicCamera: OrthographicCamera;
-  private obliqueCamera = Pb
+  private obliqueCamera : ObliqueCamera;
+  private perspectiveCamera : PerspectiveCamera;
 
   constructor(canvas: HTMLCanvasElement) {
     this.orthographicCamera = new OrthographicCamera(
@@ -15,6 +18,24 @@ export class CameraManager {
       -500,
       500
     );
+    this.obliqueCamera = new ObliqueCamera(
+      -canvas.clientWidth / 2,
+      canvas.clientWidth / 2,
+      -canvas.clientHeight / 2,
+      canvas.clientHeight / 2,
+      -500,
+      500,
+      45,
+      45
+    );
+
+    this.perspectiveCamera = new PerspectiveCamera(
+      60,
+      -500,
+      canvas.clientWidth / canvas.clientHeight,
+      500,
+    )
+
     this.camera = this.orthographicCamera;
   }
 
@@ -25,6 +46,10 @@ export class CameraManager {
   setCamera(newCamera: string) {
     if (newCamera == 'orthographic') {
       this.camera = this.orthographicCamera;
+    }else if(newCamera == 'oblique'){
+      this.camera = this.obliqueCamera;
+    }else if(newCamera == 'perspective'){
+      this.camera = this.perspectiveCamera;
     }
   }
 }
