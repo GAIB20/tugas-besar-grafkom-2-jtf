9b2406dc4199c7e6885f2eb6c29ce283e13d77d6
tobias ns <13521090@std.stei.itb.ac.id>
2024-05-17 08:18:06 +0700

    feat: fix shader, camera pos

--- src/mesh/material/phong/glsl/frag.glsl.ts
+++ src/mesh/material/phong/glsl/frag.glsl.ts
@@ -13,14 +13,15 @@ void main() {
     vec3 ambient = ambientStrength * vec3(1.0, 1.0, 1.0);
 
     vec3 norm = normalize(v_normal);
-    vec3 lightDir = normalize(normalize(vec3(1.2, 1.0, 2.0)) - v_fragPos);
+    vec3 lightDir = normalize(vec3(1.2, 1.0, 2.0) - v_fragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * vec3(1.0, 1.0, 1.0);
 
+    float specularStrength = 0.5;
     vec3 viewDir = normalize(u_viewPos - v_fragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32.0);
-    vec3 specular = spec * vec3(1.0, 1.0, 1.0);  
+    vec3 specular = specularStrength * spec * vec3(1.0, 1.0, 1.0);  
         
     vec3 result = (ambient + diffuse + specular) * u_diffuseColor;
     gl_FragColor = vec4(result, 1.0);
