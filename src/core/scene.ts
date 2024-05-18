30eef1616bfabcb24a78acc33da47e8da08e5606
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2024-05-18 18:43:16 +0700

    set type and name to scene

--- src/core/scene.ts
+++ src/core/scene.ts
@@ -1,3 +1,9 @@
 import { Object3D } from './object3D';
 
-export class Scene extends Object3D {}
+export class Scene extends Object3D {
+  constructor() {
+    super();
+    this.name = 'Scene';
+    this.type = 'scene';
+  }
+}
