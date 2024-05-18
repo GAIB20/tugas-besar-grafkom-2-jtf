bdfa19a15b20afefdc2e4c871938f9b920f7befa
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2024-05-18 18:42:42 +0700

    interfaces for json

--- src/core/interface.d.ts
+++ src/core/interface.d.ts
@@ -9,3 +9,28 @@ export interface Coordinate {
   y: number;
   z: number;
 }
+
+export interface IVector3 {
+  x: number;
+  y: number;
+  z: number;
+}
+
+export interface IObject3D {
+  name: string;
+  type: string;
+  position: IVector3;
+  rotation: IVector3;
+  scale: IVector3;
+  children: IObject3D[];
+}
+
+export interface IBoxGeometry {
+  width: number;
+  height: number;
+  depth: number;
+}
+
+export interface IMesh extends IObject3D {
+  geometry: IBoxGeometry;
+}
