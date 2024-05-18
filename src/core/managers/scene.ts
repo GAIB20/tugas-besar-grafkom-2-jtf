d377c7ba84422d106fd06b67627a971ef2ebb20d
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2024-05-18 18:44:33 +0700

    person articulated model, load articulated from json

--- src/core/managers/scene.ts
+++ src/core/managers/scene.ts
@@ -5,6 +5,8 @@ import BasicMaterial from '../../mesh/material/basic/BasicMaterial';
 import { Mesh } from '../mesh';
 import { Object3D } from '../object3D';
 import { Scene } from '../scene';
+import { IMesh, IObject3D } from '../interface';
+import PersonJSON from '../../../test-data/articulated-model/person.json';
 
 export class SceneManager {
   scene: Scene;
@@ -12,6 +14,8 @@ export class SceneManager {
   private sceneB: Scene;
   private sceneC: Scene;
   private sceneD: Scene;
+  private sceneE: Scene;
+  private sceneF: Scene;
 
   selectedMesh: Object3D;
 
@@ -52,6 +56,74 @@ export class SceneManager {
       new Mesh(new HollowBox(400, 200, 200), new BasicMaterial())
     );
 
+    // Articulated: Person
+    const head = new Mesh(new BoxGeometry(50, 50, 50), new BasicMaterial());
+    head.name = 'Head';
+    head.position.y = 150;
+
+    const body = new Mesh(new BoxGeometry(200, 250, 50), new BasicMaterial());
+    body.name = 'Body';
+
+    const leftShoulder = new Mesh(
+      new BoxGeometry(50, 25, 50),
+      new BasicMaterial()
+    );
+    leftShoulder.name = 'LeftShoulder';
+    leftShoulder.position.x = -125;
+    leftShoulder.position.y = 90;
+
+    const leftArm = new Mesh(new BoxGeometry(25, 150, 50), new BasicMaterial());
+    leftArm.name = 'LeftArm';
+    leftArm.position.x = -12.5;
+    leftArm.position.y = -87.5;
+
+    const rightShoulder = new Mesh(
+      new BoxGeometry(50, 25, 50),
+      new BasicMaterial()
+    );
+    rightShoulder.name = 'LeftShoulder';
+    rightShoulder.position.x = 125;
+    rightShoulder.position.y = 90;
+
+    const rightArm = new Mesh(
+      new BoxGeometry(25, 150, 50),
+      new BasicMaterial()
+    );
+    rightArm.name = 'RightArm';
+    rightArm.position.x = 12.5;
+    rightArm.position.y = -87.5;
+
+    const leftFoot = new Mesh(
+      new BoxGeometry(30, 200, 50),
+      new BasicMaterial()
+    );
+    leftFoot.name = 'LeftFoot';
+    leftFoot.position.x = -40;
+    leftFoot.position.y = -200;
+
+    const rightFoot = new Mesh(
+      new BoxGeometry(30, 200, 50),
+      new BasicMaterial()
+    );
+    rightFoot.name = 'RightFoot';
+    rightFoot.position.x = 40;
+    rightFoot.position.y = -200;
+
+    leftShoulder.add(leftArm);
+    rightShoulder.add(rightArm);
+    body
+      .add(head)
+      .add(leftShoulder)
+      .add(rightShoulder)
+      .add(leftFoot)
+      .add(rightFoot);
+
+    this.sceneE = new Scene().add(body);
+    this.sceneE.name = 'Person';
+
+    // Articulated Person: from JSON
+    this.sceneF = SceneManager.loadJSON(PersonJSON);
+
     this.scene = this.sceneA;
     this.selectedMesh = this.scene;
     this.setSelectedMeshOnScene();
@@ -81,6 +153,10 @@ export class SceneManager {
       this.scene = this.sceneC;
     } else if (newScene == 'D') {
       this.scene = this.sceneD;
+    } else if (newScene == 'E') {
+      this.scene = this.sceneE;
+    } else if (newScene == 'F') {
+      this.scene = this.sceneF;
     }
 
     this.setSelectedMeshOnScene();
@@ -88,5 +164,39 @@ export class SceneManager {
 
   setSelectedMesh(mesh: Mesh) {
     this.selectedMesh = mesh;
+  }
+
+  static loadJSON(object: IObject3D): Object3D {
+    switch (object.type) {
+      case 'scene':
+        const scene = new Scene();
+        scene.fromJSON(object);
+
+        object.children.forEach((child: IObject3D) => {
+          scene.add(SceneManager.loadJSON(child));
+        });
+
+        return scene;
+      case 'mesh':
+        const meshObject = object as IMesh;
+
+        const mesh = new Mesh(
+          new BoxGeometry(
+            meshObject.geometry.width,
+            meshObject.geometry.height,
+            meshObject.geometry.depth
+          ),
+          new BasicMaterial()
+        );
+        mesh.fromJSON(object);
+
+        object.children.forEach((child: IObject3D) => {
+          mesh.add(SceneManager.loadJSON(child));
+        });
+
+        return mesh;
+      default:
+        throw new Error(`Unsupported object type: ${object.type}`);
+    }
   }
 }
