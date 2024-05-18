fe21281bae9c991cd8fca229b7750d9dc0e6784e
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2024-05-18 18:43:08 +0700

    mesh to json

--- src/core/mesh.ts
+++ src/core/mesh.ts
@@ -1,5 +1,7 @@
+import { BoxGeometry } from '../mesh/geometry/basic/boxGeometry';
 import { BufferGeometry } from '../mesh/geometry/bufferGeometry';
-import ShaderMaterial from '../mesh/material/ShaderMaterial';
+import ShaderMaterial from '../mesh/material/shaderMaterial';
+import { IBoxGeometry, IMesh } from './interface';
 import { Object3D } from './object3D';
 
 export class Mesh extends Object3D {
@@ -9,7 +11,28 @@ export class Mesh extends Object3D {
   constructor(geometry: BufferGeometry, material: ShaderMaterial) {
     super();
     this.name = 'Mesh';
+    this.type = 'mesh';
     this.geometry = geometry;
     this.material = material;
+  }
+
+  // Only works for BoxGeometry
+  toJSON(): IMesh {
+    let geometryJson: IBoxGeometry;
+
+    if (this.geometry instanceof BoxGeometry) {
+      geometryJson = (this.geometry as BoxGeometry).toJSON();
+    } else {
+      geometryJson = {
+        width: 0,
+        height: 0,
+        depth: 0
+      };
+    }
+
+    return {
+      ...super.toJSON(),
+      geometry: geometryJson
+    };
   }
 }
